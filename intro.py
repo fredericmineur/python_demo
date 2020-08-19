3eda4142f5b1462840f7538d952f7f46519c7442
Frederic Mineur <frederic.mineur@vliz.be>
2020-08-20 00:57:16 +0200

    -first commit

---
+++ intro.py
@@ -0,0 +1,10 @@
+# message = 'hello world'
+# print(len(message))
+# print(message[0:5])
+
+import sys
+student = {'name': 'John', 'age': 25, 'courses': ['Math', 'compSci']}
+for value in student:
+    print(value)
+
+print(sys.path)
---
+++ script_vs.py
@@ -0,0 +1,20 @@
+import math
+import sys
+
+import requests
+
+# # print(sys.version)
+# print(sys.executable)
+
+
+# def greet(who_to_greet):
+#     greeting = 'Hello, {}'.format(who_to_greet)
+#     return greeting
+
+
+# print(greet('World'))
+# r = requests.get('https://coreyms.com')
+# print(r.status_code)
+
+name = input('What\'s your name?')
+print(f'Hello {name}')
